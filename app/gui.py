d92f0c7b0a7dbb53b45aa151c5269ecdbe5294f5
solenedietsch <solene.dietsch@gmail.com>
2025-01-11 18:29:04 +0000

    layout: resize the image element, add a file sector and centered justify

--- app/gui.py
+++ app/gui.py
@@ -5,13 +5,15 @@ def create_window():
     sg.theme('Black')
     button_color = sg.theme_background_color()
 
-    layout = [[sg.Image(key='-IMAGE-', size=(300, 300))],
+    layout = [
+        [sg.Text('Select a video to play:', size=(17, 1)), sg.InputText(key='-FILE-', enable_events=True), sg.FileBrowse(target='-FILE-')],
+        [sg.Image(key='-IMAGE-', size=(854, 480))],
         [sg.Button(image_data=button_previous, key= '-PREVIOUS-', border_width=0, button_color=button_color) ,
             sg.Button(image_data=button_play, key='-PLAY-', border_width=0, button_color=button_color),
             sg.Button(image_data=button_pause, key='-PAUSE-', border_width=0, button_color=button_color),
             sg.Button(image_data=button_next, key='-NEXT-', border_width=0, button_color=button_color)],
     ]
-    window = sg.Window("EnAcuity Player", layout)
+    window = sg.Window("EnAcuity Player", layout, element_justification='c')
 
     return window
 

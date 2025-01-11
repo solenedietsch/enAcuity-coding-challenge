5aebdb5b6f6386979c335551d9f42bd056af75b0
solenedietsch <solene.dietsch@gmail.com>
2025-01-11 13:14:17 +0000

    layout: add button previous, play, pause, next

--- app/gui.py
+++ app/gui.py
@@ -2,7 +2,12 @@ import PySimpleGUI as sg
 from data.images.output import button_play, button_pause, button_next, button_previous
 
 def create_window():
-    layout = []
+    layout = [
+        [sg.Button(image_data=button_previous, key= '-PREVIOUS-', border_width=0) ,
+            sg.Button(image_data=button_play, key='-PLAY-', border_width=0),
+            sg.Button(image_data=button_pause, key='-PAUSE-', border_width=0),
+            sg.Button(image_data=button_next, key='-NEXT-', border_width=0)],
+    ]
     window = sg.Window("EnAcuity Player", layout)
 
     return window

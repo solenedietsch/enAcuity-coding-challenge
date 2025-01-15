423a634aa07aa62f6476e6cecc74ff2f68976c35
solenedietsch <solene.dietsch@gmail.com>
2025-01-15 20:57:30 +0000

    Fix: the way you get the current frame if
    Now it always check the id from the video directly

--- app/components/video_player.py
+++ app/components/video_player.py
@@ -24,7 +24,7 @@ class VideoPlayer:
         self.ret = False
         self.frame = None
 
-        self.current_frame_id = 0
+        self.current_frame_id = self.get_current_frame_id()
         self.is_playing = False
 
     def get_nb_frames(self):
@@ -46,23 +46,21 @@ class VideoPlayer:
             raise ValueError("Error reading frame")
         return self.frame
 
+    def set_current_frame_id(self, frame_id: float):
+        self.current_frame_id = self.video_file.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+        return self.current_frame_id
 
-        return self.ret, self.frame
-
-    def set_current_frame_id(self, frame_id: int):
-        self.current_frame = self.video_file.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
-        return self.current_frame
-
-    def set_current_frame_from_id(self, frame_id: int):
-        self.set_current_frame_id(frame_id)
+    def set_current_frame_from_frame_id(self, frame_id: float):
+        # As the frame is updated using self.video_file.read() that increments the current frame id
+        # Set it to the previous one to match the right frame id
+        self.set_current_frame_id(frame_id - 1)
         self.ret, self.frame = self.video_file.read()
+
         return self.ret, self.frame
 
     def get_current_frame_id(self):
-        self.current_frame = self.video_file.get(cv2.CAP_PROP_POS_FRAMES)
-        return self.current_frame
-
         self.current_frame_id = self.video_file.get(cv2.CAP_PROP_POS_FRAMES)
+        return self.current_frame_id
 
     def set_previous_frame(self):
         # Get the current frame id when clicking on the previous button
@@ -73,6 +71,9 @@ class VideoPlayer:
         # The index must 2, as the frame id is set to -2, but displayed -1 with read.
         previous_frame_id = current_frame_id - 2
 
+        # Set the current frame to the previous frame
+        self.current_frame_id = previous_frame_id
+        self.video_file.set(cv2.CAP_PROP_POS_FRAMES, self.current_frame_id)
 
     def play_video(self):
         self.is_playing = True
